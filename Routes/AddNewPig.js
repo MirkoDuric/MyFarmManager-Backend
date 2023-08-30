7b7081b238dc75706bc4f8c4dfeda4c1b4ac7dbe
MirkoDuric <mirkodjuricczv@gmail.com>
2023-08-31 01:05:15 +0200

    Restructured DB and started doing CRUD ops.

---
+++ Routes/AddNewPig.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express.Router();
+const pool = require("../pool");
+
+module.exports = app;
--- Routes/PigInfo.js
+++ Routes/PigInfo.js
@@ -3,4 +3,23 @@ const app = express.Router();
 const pool = require("../pool");
 // U ovom fajlu vracam informacije o svinjama
 
+// ovde vracam listu svih svinja u listi po 20
+app.get("/piginfo/piglist", (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = 20;
+  const offset = (page - 1) * limit;
+
+  pool.query(
+    "SELECT serijski_broj_svinje, rasa_svinje FROM PigInfo LIMIT $1 OFFSET $2;",
+    [limit, offset],
+    (err, results) => {
+      if (err) {
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+      }
+      res.json(results.rows);
+    }
+  );
+});
+
 module.exports = app;
