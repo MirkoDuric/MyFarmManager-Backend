5843710ae5f4fac581ec94241c0f277fc6de76d9
MirkoDuric <mirkodjuricczv@gmail.com>
2023-09-01 00:36:29 +0200

    Creating new CRUD OPERATION for adding new pigs

--- Routes/AddNewPig.js
+++ Routes/AddNewPig.js
@@ -1,44 +1,63 @@
 const express = require("express");
 const app = express.Router();
 const pool = require("../pool");
+
 app.post("/dodajSvinju", (req, res) => {
-  const serijski_broj = req.body.serijski_broj;
-  const rasa = req.body.rasa;
-  const podsjetnik = req.body.podsjetnik;
-  const vakcine = req.body.vakcine;
-  const nazivVakcine = req.body.nazivVakcine;
-  const datumVakcine = req.body.datumVakcine;
+  const {
+    serijski_broj,
+    rasa,
+    tekst_podsjetnika,
+    datumPodsjetnika,
+    vakcine,
+    nazivVakcine,
+    datumVakcine,
+  } = req.body;
 
   pool.query(
-    "INSERT INTO PigInfo (serijski_broj_svinje, rasa_svinje, podsjetnik) VALUES ($1, $2, $3) RETURNING id;",
-    [serijski_broj, rasa, podsjetnik],
+    "INSERT INTO PigInfo (serijski_broj_svinje, rasa_svinje) VALUES ($1, $2) RETURNING id;",
+    [serijski_broj, rasa],
     (err, results) => {
       if (err) {
-        // handle error
-        res.status(500).json({ error: "Greška pri kreiranju svinje." });
-      } else {
-        //u varijabli ispod cuvam id koji sam proslijedio sa RETURNING id
-        const newlyCreatedId = results.rows[0].id;
-
-        if (vakcine) {
-          pool.query(
-            "INSERT INTO vakcine (pig_id, vakcina, datum) VALUES ($1, $2, $3);",
-            [newlyCreatedId, nazivVakcine, datumVakcine],
-            (err, vakcineRezultati) => {
-              if (err) {
-                // handle error
-                res
-                  .status(500)
-                  .json({ error: "Greška pri dodavanju vakcine." });
-              } else {
-                res.json({ success: true, pigId: newlyCreatedId });
-              }
-            }
-          );
-        } else {
-          res.json({ success: true, pigId: newlyCreatedId });
-        }
+        console.error("Greška prilikom kreiranja svinje:", err.message);
+        return res.status(500).json({ error: "Greška pri kreiranju svinje." });
       }
+
+      const newlyCreatedId = results.rows[0].id;
+
+      if (vakcine) {
+        pool.query(
+          "INSERT INTO vakcine (pig_id, naziv_vakcine, datum_primene) VALUES ($1, $2, $3);",
+          [newlyCreatedId, nazivVakcine, datumVakcine],
+          (err) => {
+            if (err) {
+              console.error("Greška prilikom dodavanja vakcine:", err.message);
+              return res
+                .status(500)
+                .json({ error: "Greška pri dodavanju vakcine." });
+            }
+          }
+        );
+      }
+
+      if (tekst_podsjetnika && datumPodsjetnika) {
+        pool.query(
+          "INSERT INTO podsjetnici (pig_id, tekst_podsjetnika, datumPodsjetnika) VALUES ($1, $2, $3);",
+          [newlyCreatedId, tekst_podsjetnika, datumPodsjetnika],
+          (err) => {
+            if (err) {
+              console.error(
+                "Greška prilikom dodavanja podsjetnika:",
+                err.message
+              );
+              return res
+                .status(500)
+                .json({ error: "Greška pri dodavanju podsjetnika." });
+            }
+          }
+        );
+      }
+
+      res.json({ success: true, pigId: newlyCreatedId });
     }
   );
 });
