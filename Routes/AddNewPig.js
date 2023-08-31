07b3d742d8987499d08f84de3f4436bcda06b67b
MirkoDuric <mirkodjuricczv@gmail.com>
2023-08-31 23:34:01 +0200

    Creating new CRUD OPERATION for adding new pigs

--- Routes/AddNewPig.js
+++ Routes/AddNewPig.js
@@ -1,5 +1,46 @@
 const express = require("express");
 const app = express.Router();
 const pool = require("../pool");
+app.post("/dodajSvinju", (req, res) => {
+  const serijski_broj = req.body.serijski_broj;
+  const rasa = req.body.rasa;
+  const podsjetnik = req.body.podsjetnik;
+  const vakcine = req.body.vakcine;
+  const nazivVakcine = req.body.nazivVakcine;
+  const datumVakcine = req.body.datumVakcine;
+
+  pool.query(
+    "INSERT INTO PigInfo (serijski_broj_svinje, rasa_svinje, podsjetnik) VALUES ($1, $2, $3) RETURNING id;",
+    [serijski_broj, rasa, podsjetnik],
+    (err, results) => {
+      if (err) {
+        // handle error
+        res.status(500).json({ error: "Greška pri kreiranju svinje." });
+      } else {
+        //u varijabli ispod cuvam id koji sam proslijedio sa RETURNING id
+        const newlyCreatedId = results.rows[0].id;
+
+        if (vakcine) {
+          pool.query(
+            "INSERT INTO vakcine (pig_id, vakcina, datum) VALUES ($1, $2, $3);",
+            [newlyCreatedId, nazivVakcine, datumVakcine],
+            (err, vakcineRezultati) => {
+              if (err) {
+                // handle error
+                res
+                  .status(500)
+                  .json({ error: "Greška pri dodavanju vakcine." });
+              } else {
+                res.json({ success: true, pigId: newlyCreatedId });
+              }
+            }
+          );
+        } else {
+          res.json({ success: true, pigId: newlyCreatedId });
+        }
+      }
+    }
+  );
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app = express();
 const port = process.env.PORT || 8001;
 const bodyParser = require("body-parser");
 const PigInfo = require("./Routes/PigInfo");
+const AddNewPig = require("./Routes/AddNewPig");
 app.use(bodyParser.json());
 app.use("", PigInfo);
+app.use("", AddNewPig);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
