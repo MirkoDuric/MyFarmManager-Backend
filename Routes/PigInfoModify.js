5c0831bbee150518461cab2c00cc20e6bffdbb4a
MirkoDuric <mirkodjuricczv@gmail.com>
2023-09-07 19:24:50 +0200

    Sve tabele povezane i popunjene CRUD operacijama. Nedostaje jedino sjemenjenje, ta tabela jos nije ni stvorena, cekam na vise informacija

--- Routes/PigInfoModify.js
+++ Routes/PigInfoModify.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express.Router();
+const { body, validationResult } = require("express-validator");
 const pool = require("../pool");
 //U ovom fajlu pisem U D radnje prepravljam i brisem svinje i informacije.
 
@@ -15,6 +16,7 @@ app.delete("/obrisiSvinju/:id", (req, res) => {
         return res.status(500).json({ error: "Greška pri brisanju svinje." });
       }
       if (results.rowCount === 0) {
+        console.log(err);
         return res.status(404).json({ error: "Svinja nije pronađena." });
       }
       res.json({ success: true });
@@ -22,38 +24,38 @@ app.delete("/obrisiSvinju/:id", (req, res) => {
   );
 });
 
-//update svinje
-// app.put(
-//   "/azurirajSvinju/:id",
-//   [
-//     body("serijski_broj").isInt().withMessage("Serijski broj mora biti broj."),
-//     body("rasa").isString().withMessage("Rasa mora biti tekstualni string."),
-//   ],
-//   (req, res) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() });
-//     }
+// update svinje
+app.put(
+  "/azurirajSvinju/:id",
+  [
+    body("serijski_broj").isInt().withMessage("Serijski broj mora biti broj."),
+    body("rasa").isString().withMessage("Rasa mora biti tekstualni string."),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-//     const { serijski_broj, rasa } = req.body;
+    const { serijski_broj, rasa } = req.body;
 
-//     pool.query(
-//       "UPDATE PigInfo SET serijski_broj_svinje = $1, rasa_svinje = $2 WHERE id = $3;",
-//       [serijski_broj, rasa, req.params.id],
-//       (err, results) => {
-//         if (err) {
-//           console.error("Greška prilikom ažuriranja svinje:", err.message);
-//           return res
-//             .status(500)
-//             .json({ error: "Greška pri ažuriranju svinje." });
-//         }
-//         if (results.rowCount === 0) {
-//           return res.status(404).json({ error: "Svinja nije pronađena." });
-//         }
-//         res.json({ success: true });
-//       }
-//     );
-//   }
-// );
+    pool.query(
+      "UPDATE PigInfo SET serijski_broj_svinje = $1, rasa_svinje = $2 WHERE id = $3;",
+      [serijski_broj, rasa, req.params.id],
+      (err, results) => {
+        if (err) {
+          console.error("Greška prilikom ažuriranja svinje:", err.message);
+          return res
+            .status(500)
+            .json({ error: "Greška pri ažuriranju svinje." });
+        }
+        if (results.rowCount === 0) {
+          return res.status(404).json({ error: "Svinja nije pronađena." });
+        }
+        res.json({ success: true });
+      }
+    );
+  }
+);
 
 module.exports = app;
